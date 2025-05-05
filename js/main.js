54dbdbcb349439b35894c171264b4db3f2124880
dionysiusmukha <dionysiusmukha@gmail.com>
2025-05-05 22:35:18 +0300

    create delete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let taskCounter = 0;
+let taskEmpty = 0;
 document.getElementById('main-btn').addEventListener('click', addTask);
 
 
@@ -6,22 +7,46 @@ function addTask() {
     const container = document.getElementById('container-main');
     taskCounter++;
 
-
-
     const task = document.createElement('div');
     task.classList.add('task');
     container.appendChild(task);
+
+
     const nameAndCategory = document.createElement('div');
     nameAndCategory.classList.add('name-and-category');
     task.appendChild(nameAndCategory);
+
     const nameTask = document.createElement('p');
     nameTask.classList.add('name-task');
     nameAndCategory.appendChild(nameTask);
+    nameTask.setAttribute('contenteditable','true')
     nameTask.textContent = `Name ${taskCounter}`;
+    nameTask.addEventListener('blur', function () {
+       if (this.textContent.trim() === '') {
+           taskEmpty++;
+           this.textContent = `Name Empty${taskEmpty}`;
+       }
+    });
+    nameTask.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            this.blur();
+        }
+    });
+    const MAX_LENGTH = 18; // Максимальное количество символов
+
+    nameTask.addEventListener('input', function() {
+        if (this.textContent.length > MAX_LENGTH) {
+            this.textContent = this.textContent.substring(0, MAX_LENGTH);
+            alert(`Максимальная длина - ${MAX_LENGTH} символов`);
+        }
+    });
+
     const categoryTask = document.createElement('p');
     categoryTask.classList.add('category');
     nameAndCategory.appendChild(categoryTask);
     categoryTask.textContent = `Category ${taskCounter}`;
+
     const containerForCards = document.createElement('div');
     containerForCards.classList.add('container-for-cards');
     task.appendChild(containerForCards);
@@ -32,6 +57,12 @@ function addTask() {
     textElemnt.classList.add('textInRect');
     task.appendChild(textElemnt);
     textElemnt.addEventListener('click',addCard);
+
+    const deleteCardBtn = document.createElement('img');
+    deleteCardBtn.setAttribute('id', 'delete-card-btn');
+    deleteCardBtn.setAttribute('src', 'img/bin.svg')
+    task.appendChild(deleteCardBtn);
+    deleteCardBtn.addEventListener('click', deleteTask);
 }
 
 
@@ -41,4 +72,9 @@ function addCard() {
     const card = document.createElement('div');
     card.classList.add('cards');
     containerForCards.insertBefore(card,containerForCards.firstChild);
+}
+
+function deleteTask() {
+    const taskElement = this.parentNode;
+    taskElement.remove();
 }
